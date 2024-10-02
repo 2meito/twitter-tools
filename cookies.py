1058d7ee13c3c56bedc4881167c30069b817292f
2meito <22meito@gmail.com>
2024-10-02 17:41:06 -0400

    Fix cookies.py

--- cookies.py
+++ cookies.py
@@ -2,9 +2,13 @@ def get(file_path):
     cookies = {'auth_token': None, 'ct0': None}
     with open(file_path) as file:
         for line in file:
-            if line.isspace() or line.startswith('#'):
+            if line.isspace():
+                continue
+            if line.startswith('#'):
                 continue
             cookie = line.split()
+            if cookie[0] not in ['.x.com', '.twitter.com']:
+                continue
             if cookie[5] == 'auth_token':
                 cookies['auth_token'] = cookie[6]
             if cookie[5] == 'ct0':
