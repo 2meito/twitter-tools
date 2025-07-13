5348d4d5330b8437adb4da449d055f33452d9704
2meito <22meito@gmail.com>
2025-07-13 05:26:06 -0400

    Swap to twikit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ```bash
-main.py USERNAMES... --cookies "$HOME/path/to/cookies.txt"
+follows.py USERNAMES... --username username --password password --save-cookies "/path/to/save/cookies/file.ext"
+```
+OR
+```bash
+follows.py USERNAMES... --cookies "/path/to/cookies/file.ext" 
 ```
--- follows.py
+++ follows.py
@@ -1,42 +1,57 @@
-import logging
+import argparse
+import asyncio
+import datetime
 import json
-from typing import List
+from pathlib import Path
+from twikit import Client
 
-from twitter.scraper import Scraper
 
-def get(scraper: Scraper, user_ids: List[int]):
-    users = []
+async def get_friends(client, user):
+    friends = await client.get_user_following(user.id, count=50)
+    count = len(friends)
+    while friends:
+        for friend in friends:
+            yield friend
+        friends = await friends.next()
+        count += len(friends)
+        print(f'processed {count} friends...')
 
-    for response in scraper.following(user_ids):
-        for instruction in response['data']['user']['result']['timeline']['timeline']['instructions']:
-            if instruction['type'] != 'TimelineAddEntries':
-                continue
-            for entry in instruction['entries']:
-                content = entry['content']
-                if content['entryType'] != 'TimelineTimelineItem':
-                    continue
-                itemContent = content['itemContent']
-                if itemContent['__typename'] != 'TimelineUser':
-                    continue
 
-                user_results = itemContent['user_results']
-                if not user_results:
-                    continue
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('screen_names', nargs='+', type=str)
+    parser.add_argument('--username', required=False, type=str)
+    parser.add_argument('--password', required=False, type=str)
+    parser.add_argument('--cookies', required=False, type=str)
+    parser.add_argument('--save-cookies', required=False, type=str)
+    args = parser.parse_args()
 
-                result = user_results['result']
+    client = Client('en-US')
+    if args.cookies:
+        client.load_cookies(args.cookies)
+    elif args.username and args.password:
+        await client.login(
+            auth_info_1=args.username,
+            password=args.password,
+        )
+        if args.save_cookies:
+            with open(args.save_cookies, 'w') as file:
+                file.write(json.dumps(client.get_cookies()))
+    else:
+        raise "No credentials or cookies supplied."
 
-                legacy = result['legacy']
-
-                rest_id = result['rest_id']
-                screen_name = legacy['screen_name']
-                name = legacy['name']
-
-                users.append({'id': rest_id, 'screen_name': screen_name, 'name': name})
-
-    return users
-
-def parse_json(file_path):
-    with open(file_path) as fp:
-        users = json.load(fp)
+    Path('out').mkdir(parents=True, exist_ok=True)
+    users = [await client.get_user_by_screen_name(screen_name) for screen_name in args.screen_names]
     for user in users:
-        print(f'https://twitter.com/{user['screen_name']}', end=' ')
+        friend_list = []
+        base_path = f'out/{user.screen_name}-{datetime.datetime.now().strftime("%Y%m%d.%H%M%S-%f")}'
+        with open(f'{base_path}.txt', 'w') as file:
+            async for friend in get_friends(client, user):
+                friend_list.append({'id': friend.id, 'screen_name': friend.screen_name, 'name': friend.name})
+                file.write(f'https://twitter.com/{friend.screen_name}\n')
+        with open(f'{base_path}.json', 'w') as file:
+            file.write(json.dumps(friend_list))
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
